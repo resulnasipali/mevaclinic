1c42e564f03ca3d8c86531ab37014022130414c9
MevaClinic <deploy@mevaclinic.com>
2026-06-14 15:12:17 +0300

    fix: implement server-level 301 redirects for legacy service and page slugs

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,51 @@ const nextConfig: NextConfig = {
         destination: 'https://www.mevaclinic.com/:path*',
         permanent: true,
       },
+      {
+        source: '/:lang(en|de|ro|es|it|ru|fr)/hair-transplant',
+        destination: '/:lang/treatments/meva-mixed-hair',
+        permanent: true,
+      },
+      {
+        source: '/:lang(en|de|ro|es|it|ru|fr)/gastric-sleeve',
+        destination: '/:lang/treatments/gastric-sleeve',
+        permanent: true,
+      },
+      {
+        source: '/:lang(en|de|ro|es|it|ru|fr)/oncology',
+        destination: '/:lang/treatments/smart-oncology-drugs',
+        permanent: true,
+      },
+      {
+        source: '/:lang(en|de|ro|es|it|ru|fr)/packages/gastric-balloon',
+        destination: '/:lang/treatments/gastric-balloon',
+        permanent: true,
+      },
+      {
+        source: '/ro/piezo-rhinoplasty',
+        destination: '/ro/treatments/piezo-rhinoplasty',
+        permanent: true,
+      },
+      {
+        source: '/:lang(en|de|ro|es|it|ru|fr)/smart-oncology-drugs',
+        destination: '/:lang/treatments/smart-oncology-drugs',
+        permanent: true,
+      },
+      {
+        source: '/ro/despre-noi',
+        destination: '/ro/about-us',
+        permanent: true,
+      },
+      {
+        source: '/:lang(en|de|ro|es|it|ru|fr)/comparatie-medicala',
+        destination: '/:lang/medical-comparison',
+        permanent: true,
+      },
+      {
+        source: '/:lang(en|de|ro|es|it|ru|fr)/politica-confidentialitate',
+        destination: '/:lang/privacy-policy',
+        permanent: true,
+      }
     ];
   },
 };
